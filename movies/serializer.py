14ba1ad26989b8c16c33051e09557e45812032e6
Ismoilkhan22 <143698453+Ismoilkhan22@users.noreply.github.com>
2024-03-17 11:59:13 +0500

    8-commit

--- movies/serializer.py
+++ movies/serializer.py
@@ -3,7 +3,30 @@ from rest_framework import serializers
 from movies.models import Movie, Category, Review
 
 
+class FilterReviewListSerializer(serializers.ListSerializer):
+    """
+    commit write
+    """
+
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
+class RecursiveSerializer(serializers.Serializer):
+    """
+    commit write
+    """
+
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
+
+
 class MovieSerializer(serializers.ModelSerializer):
+    """
+    commit write
+    """
     class Meta:
         model = Movie
         fields = ("title", "tagline")
@@ -23,10 +46,12 @@ class ReviewSerializer(serializers.ModelSerializer):
     """
         Review serializer
     """
+    children = RecursiveSerializer(many=True)
 
     class Meta:
+        list_serializer_class = FilterReviewListSerializer
         model = Review
-        fields = ("name", "text", "parent")
+        fields = ("name", "text", "children")
 
 
 class MovieDetailSerializer(serializers.ModelSerializer):
