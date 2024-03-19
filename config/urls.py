bb3b3ae8c4c693b7ad5de0c08a34d5f8f5c7143d
Ismoilkhan22 <143698453+Ismoilkhan22@users.noreply.github.com>
2024-03-19 12:55:36 +0500

    swagger

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +29,5 @@ urlpatterns = [
 
 
 ]
+
+urlpatterns += doc_urls
